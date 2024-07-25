8d91b10bae189dcf5f4474b1d4a9354da822e226
Saud Malik <saud@Sauds-MacBook-Pro.local>
2024-07-25 22:58:58 +0500

    add login functionality

--- fb-bot/background.js
+++ fb-bot/background.js
@@ -2,9 +2,9 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
-  });
-});
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     files: ['content.js']
+//   });
+// });
--- fb-bot/popup.js
+++ fb-bot/popup.js
@@ -1,10 +1,24 @@
 document.getElementById('activate').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "login" }, (response) => {
-      console.log("Fetched Data: ", response.data);
-      document.getElementById("output").textContent = JSON.stringify(response.data, null, 2);
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    try {
+      const response = await fetch('http://localhost:3000/data');
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      const data = await response.json();
+      console.log(data);
+    
+      if (data.length > 0) {
+        const credentials = { action: 'login', email: data[0]['email'], password: data[0]['password'] };
+        console.log(credentials);
+        chrome.tabs.sendMessage(tabs[0].id, credentials, () => {
+          console.log('Message sent with email and password');
+        });
+      } else {
+        console.error('No data received from the API');
+      }
+    } catch (error) {
+      console.error('There has been a problem with your fetch operation:', error);
+    }
   });
 });
-
-
