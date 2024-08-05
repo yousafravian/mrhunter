b9d086ed51aaad2fd0fae7effc08f3cb09eb6bf3
Saud Malik <saud@Sauds-MacBook-Pro.local>
2024-08-05 18:55:26 +0500

    create last scraped user

--- fb-bot/background.js
+++ fb-bot/background.js
@@ -2,28 +2,29 @@ let profiles = [];
 let pages = [];
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  if (message.action === "fetchData") {
+  if (message.action === 'fetchData') {
     try {
-      const [credentialsResponse, pagesResponse, scrapedUsersCountResponse] = await Promise.all([
-        fetch("http://localhost:3000/credentials"),
-        fetch("http://localhost:3000/pages"),
-        fetch("http://localhost:3000/scraped_users/groupByPageDomain")
-      ]);
+      const [credentialsResponse, pagesResponse, scrapedUsersCountResponse] =
+        await Promise.all([
+          fetch('http://localhost:3000/credentials'),
+          fetch('http://localhost:3000/pages'),
+          fetch('http://localhost:3000/scraped_users/groupByPageDomain')
+        ]);
 
       if (!credentialsResponse.ok) {
         throw new Error(
-          "Network response was not ok: " + credentialsResponse.statusText
+          'Network response was not ok: ' + credentialsResponse.statusText
         );
       }
       if (!pagesResponse.ok) {
         throw new Error(
-          "Network response was not ok: " + pagesResponse.statusText
+          'Network response was not ok: ' + pagesResponse.statusText
         );
       }
 
       if (!scrapedUsersCountResponse.ok) {
         throw new Error(
-          "Network response was not ok: " + pagesResponse.statusText
+          'Network response was not ok: ' + pagesResponse.statusText
         );
       }
 
@@ -34,14 +35,14 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
       ]);
 
       if (profiles.length === 0) {
-        throw new Error("No data received from the API");
+        throw new Error('No data received from the API');
       }
       if (pages.length === 0) {
-        throw new Error("No data received from the API");
+        throw new Error('No data received from the API');
       }
-      
+
       chrome.runtime.sendMessage({
-        action: "data",
+        action: 'data',
         profiles: profiles,
         pages: pages,
         scrapedUsersCount: scrapedUsersCount
@@ -50,22 +51,31 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
       console.log(pages);
       console.log(scrapedUsersCount);
     } catch (error) {
-      console.error("Error:", error);
+      console.error('Error:', error);
     }
-  } else if (message.action === "startScraping" && message.link) {
-    chrome.tabs.create({ url: message.link }, function(tab) {
-      chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-          if (tabId === tab.id && changeInfo.status === 'complete') {
-              chrome.tabs.sendMessage(tab.id, { action: "startScraping" });
-          }
+  } else if (message.action === 'startScraping' && message.link) {
+    chrome.tabs.create({ url: message.link }, function (tab) {
+      const tabId = tab.id;
+    
+      function onUpdatedListener(updatedTabId, changeInfo, updatedTab) {
+        if (updatedTabId === tabId && changeInfo.status === 'complete') {
+          chrome.tabs.sendMessage(tabId, { action: 'startScraping' });
+          // Remove the listener after the message is sent
+          chrome.tabs.onUpdated.removeListener(onUpdatedListener);
+        }
+      }
+    
+      chrome.tabs.onUpdated.addListener(onUpdatedListener);
+    });
+    
+  } else if (message.action === 'login' && message.email) {
+    const profile = profiles.find((profile) => profile.email === message.email);
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: 'login',
+        email: profile.email,
+        password: profile.password
       });
-  });
-
-  } else if (message.action === "login" && message.email) {
-    const profile = profiles.find(profile => profile.email === message.email);
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "login", email: profile.email, password: profile.password});
     });
   }
 });
-
--- fb-bot/page_script.js
+++ fb-bot/page_script.js
@@ -40,44 +40,105 @@ async function getAllProfileLinks(startNum, foundProfiles) {
   const pageDomain = window.location.href;
 
   while (true) {
-    const xpath = `/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[${num}]/div/div/div/div/div/div/div/div/div/div[13]/div/div/div[2]/div/div[2]/div/div[1]/span/h2/span/strong[1]/span/a`;
-    const profile = getElementByXPath(xpath);
+    try {
+      const profileXpath = `/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[${num}]/div/div/div/div/div/div/div/div/div/div[13]/div/div/div[2]/div/div[2]/div/div[1]/span/h2/span/strong[1]/span/a`;
+      const postXpath = `/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[${num}]/div/div/div/div/div/div/div/div/div/div[13]/div/div/div[3]/div/div/span/div`;
 
-    if (profile && !foundProfiles.has(profile.href)) {
-      profileLinks.push(profile.href);
-      foundProfiles.add(profile.href);
-      console.log(`Found profile link: ${profile.href} at num: ${num}`);
-      await sendProfileLinksToServer(profile.href, pageDomain);
+      const profile = getElementByXPath(profileXpath);
 
-    } else {
-      console.log(`No profile found at num: ${num}`);
+      if (profile && !foundProfiles.has(profile.href)) {
+        let postContent = '';
+        const post = getElementByXPath(postXpath);
+        if (post) {
+          postContent = post.textContent;
+        }
+
+        profileLinks.push(profile.href);
+        foundProfiles.add(profile.href);
+        console.log(`Found profile link: ${profile.href} at num: ${num}`);
+
+        await sendProfileLinksToServer(profile.href, pageDomain, postContent);
+
+        if (num === 2) {
+          await sendLastSrapedUserToServer(profile.href, pageDomain);
+        }
+      } else {
+        console.log(`No profile found at num: ${num}`);
+        break;
+      }
+
+      num++;
+    } catch (error) {
+      console.error(`Error processing profile at num: ${num}`, error);
       break;
     }
-    num++;
   }
 
   return { profileLinks, lastProcessedNum: num - 1 };
 }
 
-async function sendProfileLinksToServer(profileLink, pageDomain) {
+
+async function sendProfileLinksToServer(profileLink, pageDomain, postContent) {
   try {
-    const response = await fetch("http://localhost:3000/scraped_users", {
-      method: "POST",
+    const response = await fetch('http://localhost:3000/scraped_users', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ profileLink, pageDomain }),
+      body: JSON.stringify({ profileLink, pageDomain, postContent })
     });
 
     if (response.ok) {
-      console.log("Profile link successfully sent to the server");
+      console.log('Profile link successfully sent to the server');
     } else {
-      console.error("Failed to send profile link to the server");
+      console.error('Failed to send profile link to the server');
     }
   } catch (error) {
-    console.error("Error sending profile links to the server:", error);
+    console.error('Error sending profile links to the server:', error);
   }
 }
+
+async function sendLastSrapedUserToServer(profileLink, pageDomain) {
+  try {
+    const response = await fetch(
+      'http://localhost:3000/scraped_users/lastScrapedUser',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ profileLink, pageDomain })
+      }
+    );
+
+    if (response.ok) {
+      console.log('last scraped user updated');
+    } else {
+      console.error('Failed to send profile link to the server');
+    }
+  } catch (error) {
+    console.error('Error sending profile links to the server:', error);
+  }
+}
+
+async function getLastScrapedUserFromServer() {
+  try {
+    const response = await fetch('http://localhost:3000/scraped_users/lastScrapedUser');
+    
+    if (!response.ok) {
+      throw new Error('Failed to fetch the last scraped user from the server');
+    }
+
+    const lastScrapedUsers = await response.json();
+    console.log('Last scraped user fetched successfully');
+    
+    return lastScrapedUsers;
+  } catch (error) {
+    console.error('Error fetching the last scraped user from the server:', error);
+    return null; // or any default value you prefer
+  }
+}
+
 
 // Main function to scroll and extract profile links
 async function extractProfileLinks() {
@@ -87,7 +148,7 @@ async function extractProfileLinks() {
   let noNewProfileAttempts = 0; // Counter for attempts with no new profiles
 
   // Initial extraction without scrolling
-  console.log("Extracting initial visible profiles");
+  console.log('Extracting initial visible profiles');
   let { profileLinks, lastProcessedNum: newLastProcessedNum } =
     await getAllProfileLinks(2, allProfileLinks);
   profileLinks.forEach((link) => allProfileLinks.add(link));
@@ -99,7 +160,7 @@ async function extractProfileLinks() {
     await scrollByAmount(scrollAmount);
 
     // Wait for 2 seconds after scrolling
-    console.log("Waiting for 2 seconds after scrolling");
+    console.log('Waiting for 2 seconds after scrolling');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     ({ profileLinks, lastProcessedNum: newLastProcessedNum } =
@@ -112,7 +173,7 @@ async function extractProfileLinks() {
       );
       if (noNewProfileAttempts >= maxScrollAttempts) {
         console.log(
-          "Reached maximum scroll attempts with no new profiles. Stopping."
+          'Reached maximum scroll attempts with no new profiles. Stopping.'
         );
         break; // Stop if no new profiles found after max attempts
       }
@@ -131,40 +192,36 @@ async function extractProfileLinks() {
     await new Promise((resolve) => setTimeout(resolve, scrollDelay));
   }
   const uniqueProfileLinks = Array.from(allProfileLinks);
-  
-
 
   // Convert Set to Array to get unique profile links
-  console.log("Finished extracting profile links.");
+  console.log('Finished extracting profile links.');
   return uniqueProfileLinks;
 }
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "startScraping") {
-      // Start the process after a delay to ensure the page is fully loaded
-      setTimeout(() => {
-        console.log("Starting profile link extraction");
-        extractProfileLinks().then((profileLinks) => {
-          console.log("All profile links:", profileLinks);
-        });
-      }, 5000);
-    } else if (message.action === "login") {
-      console.log("fired from page script");
-      const form = document.querySelector(
-        'form._9vtf[data-testid="royal_login_form"]'
-      );
-      if (form) {
-        const emailField = form.querySelector('input[name="email"]');
-        const passwordField = form.querySelector('input[name="pass"]');
-        if (emailField && passwordField) {
-          emailField.value = message.email;
-          passwordField.value = message.password;
-          form.submit();
-        } else {
-          console.error("Email or password field not found");
-        }
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'startScraping') {
+    // Start the process after a delay to ensure the page is fully loaded
+    setTimeout(() => {
+      console.log('Starting profile link extraction');
+      extractProfileLinks().then((profileLinks) => {
+        console.log('All profile links:', profileLinks);
+      });
+    }, 5000);
+  } else if (message.action === 'login') {
+    const form = document.querySelector('form');
+    if (form) {
+      console.log('form found');
+      const emailField = form.querySelector('input[name="email"]');
+      const passwordField = form.querySelector('input[name="pass"]');
+      if (emailField && passwordField) {
+        emailField.value = message.email;
+        passwordField.value = message.password;
+        form.submit();
       } else {
-        console.error("Login form not found");
+        console.error('Email or password field not found');
       }
+    } else {
+      console.error('Login form not found');
     }
-  });
+  }
+});
